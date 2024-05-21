a1cebf205e26cae8a19e22ebe618539467f57c7c
Maxim Kazantsev <maximkazantsev1997@icloud.com>
2024-05-22 02:06:26 +0700

    windows

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,10 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
+from birthday.models import Birthday
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+
+class HomePage(TemplateView):
+    # В атрибуте template_name обязательно указывается имя шаблона,
+    # на основе которого будет создана возвращаемая страница.
+    template_name = 'pages/index.html' 

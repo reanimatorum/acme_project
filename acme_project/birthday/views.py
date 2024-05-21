210d5486b0096b830d3f684da092748a3720b3a8
Maxim Kazantsev <maximkazantsev1997@icloud.com>
2024-05-21 07:08:45 +0700

    windows

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,44 +1,41 @@
-from django.core.paginator import Paginator
+# birthday/views.py
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy
 
-
 from .forms import BirthdayForm
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-class BirthdayMixin:
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-
-
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
-    model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
-    ordering = 'id'
-    # ...и даже настройки пагинации:
-    paginate_by = 10
-
-
 class BirthdayDetailView(DetailView):
-    pass
+    model = Birthday
 
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
